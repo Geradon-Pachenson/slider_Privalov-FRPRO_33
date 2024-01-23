35292036d22019d4338f3dbe7dd235d76b944ed0
Geradon Pachenson <pacheniy@mail.ru>
2024-01-23 11:27:13 +0500

    finish

--- script/script.js
+++ script/script.js
@@ -1,22 +1,25 @@
-let images = [{
+const slides = [{
         url: "images/jpeg/rostov-on-don_Admiral.jpg",
         city: 'Rostov-on-Don LCD admiral',
         repairTime: '3.5 months',
         apartmentArea: '81 m2',
+        title: 'Rostov-on-Don, Admiral',
     }, {
         url: "images/jpeg/sochi-thieves.jpg",
         city: 'Sochi Thieves',
         repairTime: '4 months',
         apartmentArea: '105 m2',
+        title: 'Sochi Thieves',
     }, {
         url: "images/jpeg/rostov-on-don_patriotic.jpg",
         city: 'Rostov-on-Don Patriotic',
         repairTime: '3 months',
         apartmentArea: '93 m2',
+        title: 'Rostov-on-Don Patriotic',
     }];
 
 function getSlider() {
-    if (!images || !images.length) return;
+    if (!slides || !slides.length) return;
     
     let sliderImg = document.querySelector(".slider__img");
     let sliderArrows = document.querySelector(".slider__arrows");
@@ -29,8 +32,8 @@ function getSlider() {
     initTitles();
     
     function initImages() {
-        images.forEach((image, index) => {
-        let imageDiv = `<div class="image n${index} ${index === 0? "active" : ""}" style="background-image:url(${images[index].url});" data-index="${index}"></div>`;
+        slides.forEach((image, index) => {
+        let imageDiv = `<div class="image n${index} ${index === 0? "active" : ""}" style="background-image:url(${slides[index].url});" data-index="${index}"></div>`;
         sliderImg.innerHTML += imageDiv;
         });
     }
@@ -41,9 +44,9 @@ function getSlider() {
             let curNumber = +sliderImg.querySelector(".active").dataset.index;
             let nextNumber;
             if (arrow.classList.contains("left")) {
-            nextNumber = curNumber === 0? images.length - 1 : curNumber - 1;
+            nextNumber = curNumber === 0? slides.length - 1 : curNumber - 1;
             } else {
-            nextNumber = curNumber === images.length - 1? 0 : curNumber + 1;
+            nextNumber = curNumber === slides.length - 1? 0 : curNumber + 1;
             }
             moveSlider(nextNumber);
         });
@@ -61,7 +64,7 @@ function getSlider() {
     }
 
     function initDots() {
-        images.forEach((image, index) => {
+        slides.forEach((slides, index) => {
         let dot = `<div class="slider__dots-item n${index} ${index === 0? "active" : ""}" data-index="${index}"></div>`;
         sliderDots.innerHTML += dot;
         });
@@ -73,28 +76,25 @@ function getSlider() {
     }
     
     function initTitles() {
-        images.forEach((image, index) => {
-        let item = `<li class="slider__picture-item n${index} ${index === 0? "active" : ""}" data-index="${index}"><span class="slider__picture-text">Rostov-on-Don, Admiral</span></li>`;
-        sliderTitle.innerHTML += item;
+        slides.forEach((slides, index) => {
+            let item = `<li class="slider__picture-item n${index} ${index === 0 ? "active" : ""}" data-index="${index}"><span class="slider__picture-text">${slides.title}</span></li>`;
+            sliderTitle.innerHTML += item;
         });
-        let itemSecond = sliderTitle.querySelectorAll(".slider__picture-text")[1];
-        itemSecond.textContent = "Sochi Thieves";
-        let itemThird = sliderTitle.querySelectorAll(".slider__picture-text")[2];
-        itemThird.textContent = "Rostov-on-Don Patriotic";
+    
         sliderTitle.querySelectorAll(".slider__picture-item").forEach(item => {
-        item.addEventListener("click", function() {
-            moveSlider(this.dataset.index);
+            item.addEventListener("click", function () {
+                moveSlider(this.dataset.index);
+            })
         })
-        })
-    }
+    }  
 
     function changeSpan(num) {
         let sliderColumnCity = document.querySelector(".column-city");
         let sliderColumnArea = document.querySelector(".column-area");
         let sliderColumnTime = document.querySelector(".column-time");
-        sliderColumnCity.innerText = (images[num].city);
-        sliderColumnArea.innerText = (images[num].apartmentArea);
-        sliderColumnTime.innerText = (images[num].repairTime);
+        sliderColumnCity.innerText = (slides[num].city);
+        sliderColumnArea.innerText = (slides[num].apartmentArea);
+        sliderColumnTime.innerText = (slides[num].repairTime);
     }
 }
 
